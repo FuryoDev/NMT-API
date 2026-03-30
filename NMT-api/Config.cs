77e88404efb17ce3df08995cb1265e987b839953
Solaiman Moujib <99276006+FuryoDev@users.noreply.github.com>
2026-03-30 13:25:09 +0200

    Init

---
+++ NMT-api/Config.cs
@@ -0,0 +1,34 @@
+﻿using Library_Common;
+using Library_Common.SharedObjects.Monitoring;
+
+namespace NMT_api
+{
+    internal static class Config
+    {
+        internal static ApplicationInfo Application = new(
+            "TODOX - The group to which belongs the application (ESB, Video, Audio, AI, Monitoring, etc.)",
+            "TODOX - The application name",
+            SharedGetter.GetInstance_API(SharedGetter.GetHost()), // TODOX - The Primary Key of the application is the couple [Name + Instance]
+            "TODOX - The description of the application",
+            "v1.0.1"); // TODOX - The version (not the same as the Assembly Version)
+        internal static class Related
+        {
+            internal static readonly List<Application> Applications =
+                [
+                    new Application("Pacemaker API", SharedConfig.Pacemaker_API.BaseAddress().ToString()),
+                    new Application("TODOX - Any Related API", "TODOX - Its endpoint")
+                ];
+            internal static readonly List<Database> Databases = // TODOX - The related Databases
+            [
+                SharedConfig.SQLServer.Logs.Database()
+            ];
+            internal static readonly List<Storage> Storages = []; // TODOX - The related Storages
+        }
+        internal static class Security
+        {
+            internal static readonly string Issuer = Application.Name;
+            private const string SecretKey_Application = "TODOX_SecretKey";
+            internal static readonly string SecretKey_Instance = SharedGetter.GetUniqueSecretKey(SecretKey_Application, Issuer);
+        }
+    }
+}
