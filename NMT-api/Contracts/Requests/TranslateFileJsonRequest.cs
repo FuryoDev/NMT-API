e3ac5821b829bf51bd18c986558e1608e3eab9a0
Solaiman Moujib <99276006+FuryoDev@users.noreply.github.com>
2026-04-08 11:43:58 +0200

    Favor faithful full translations and remove preprocessing hooks

--- NMT-api/Contracts/Requests/TranslateFileJsonRequest.cs
+++ NMT-api/Contracts/Requests/TranslateFileJsonRequest.cs
@@ -14,11 +14,8 @@ public class TranslateFileJsonRequest
     public string TargetLanguage { get; set; } = "en";
 
     [FromForm(Name = "maxNewTokens")]
-    public int MaxNewTokens { get; set; } = 256;
+    public int MaxNewTokens { get; set; } = 512;
 
     [FromForm(Name = "numBeams")]
-    public int NumBeams { get; set; } = 4;
-
-    [FromForm(Name = "preprocess")]
-    public bool Preprocess { get; set; } = true;
+    public int NumBeams { get; set; } = 5;
 }
--- NMT-api/Services/Srt/SrtBlock.cs
+++
@@ -1,8 +0,0 @@
-namespace NMT_api.Services.Srt;
-
-public class SrtBlock
-{
-    public int Index { get; set; }
-    public string TimeRange { get; set; } = string.Empty;
-    public List<string> Lines { get; set; } = [];
-}
