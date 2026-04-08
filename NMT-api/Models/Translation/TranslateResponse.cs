9ffc3e8b991e636e89d8233670d53d4fdf92f075
Solaiman Moujib <99276006+FuryoDev@users.noreply.github.com>
2026-04-08 11:57:08 +0200

    cleaning -> apply gitignore

--- NMT-api/Models/Translation/TranslateResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace NMT_api.Models.Translation;
-
-public class TranslateResponse
-{
-    public string TranslatedText { get; set; } = string.Empty;
-    public string Device { get; set; } = "cpu";
-    public int DurationMs { get; set; }
-    public List<string>? Preprocessing { get; set; }
-}
